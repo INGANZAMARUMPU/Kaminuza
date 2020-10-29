fa3cbb47802946042db3a8509cb1471bd8a5d366
konstrictor <jonkur96@gmail.com>
2020-10-29 20:38:55 +0200

    vue2 empty project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="container">
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  margin-top: 60px;
+}
+</style>
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+
+import Studform from './components/Studform.vue';
+import Courseform from './components/Courseform.vue';
+import Markform from './components/Markform.vue';
+
+export const routes = [
+    { path: '/student-form', component: Studform },
+    { path: '/course-form', component: Courseform },
+    { path: '/mark-form', component: Markform },
+];
